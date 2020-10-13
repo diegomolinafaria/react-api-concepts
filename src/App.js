333b7f699936176da67172be5dd285f401558a2e
Diego Molina <dm@diegomolina.com.br>
2020-10-13 21:37:42 +0100

    React axios and resquests concepts

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import api from './services/api'
 import "./styles.css";
 
 function App() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+      api.get('repositories').then(res => {
+        setRepositories(res.data);
+      });
+    }, []);
+    
   async function handleAddRepository() {
-    // TODO
+    const response =  await api.post('repositories', {
+      'title': `Novo repositório ${Date.now()}`,
+    });
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`).then(res => {
+      setRepositories(repositories.filter(
+        r => r.id !== id
+      ));
+    });
   }
 
   return (
-    <div>
+    <>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository => {
+          return (<li key={repository.id}>
+            {repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
-        </li>
+        </li>)
+        })}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+    </>
   );
 }
 
